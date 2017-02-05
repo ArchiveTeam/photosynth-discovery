05d067143204acf0493dd5462bd229724d494291
Arkiver2 <Arkiver@hotmail.com>
2017-02-05 03:28:19 +0100

    add TODO

--- pipeline.py
+++ pipeline.py
@@ -94,11 +94,11 @@ class PrepareDirectories(SimpleTask):
 
         os.makedirs(dirname)
 
-#        item["item_dir"] = dirname
-#        item["tar_file_base"] = "%s-%s-%s" % (
-#            self.warc_prefix, escaped_item_name,
-#            time.strftime("%Y%m%d-%H%M%S")
-#        )
+#        item["item_dir"] = dirname TODO
+#        item["tar_file_base"] = "%s-%s-%s" % ( TODO
+#            self.warc_prefix, escaped_item_name, TODO
+#            time.strftime("%Y%m%d-%H%M%S") TODO
+#        ) TODO
 
 
 class MoveFiles(SimpleTask):
